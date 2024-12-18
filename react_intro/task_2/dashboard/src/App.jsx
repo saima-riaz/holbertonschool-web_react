bc0cdfb9a0f94c1af68934d3717104e5bc79027c
saima-riaz <saimariaz1422@gmail.com>
2024-12-18 16:09:55 +0100

    updated app.jsx file by adding label input and button

---
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -0,0 +1,35 @@
+import holbertonLogo from "./assets/holberton-logo.jpg";
+import { getCurrentYear, getFooterCopy } from "./utils";
+import Notifications from "./Notifications";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="root-Notifications">
+        <Notifications />
+      </div>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+
+       { /* label section  */}
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" placeholder="Enter your email"></input> 
+        <br/>
+        <label htmlFor="password">Password:</label>
+        <input type="Password" id="password" placeholder="Enter your password"></input> <br/>
+        {/* label section  */}
+        <button role="button" type="submit">OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getCurrentYear()} {getFooterCopy(true)}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
