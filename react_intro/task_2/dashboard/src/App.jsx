7dc92d977cc0ab95c255227c6662bef413bce98e
saima-riaz <saimariaz1422@gmail.com>
2024-12-19 00:05:40 +0100

    updated code for label and button

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -1,13 +1,12 @@
 import holbertonLogo from "./assets/holberton-logo.jpg";
-import { getCurrentYear, getFooterCopy } from './utils.js'
-import Notifications from "./Notifications.jsx";
-import './Notifications.css'
-import './App.css'
+import { getCurrentYear, getFooterCopy } from "./utils";
+import Notifications from "./Notifications";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="root-notifications">
+      <div className="root-Notifications">
         <Notifications />
       </div>
       <div className="App-header">
@@ -16,17 +15,25 @@ function App() {
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="pass">Password</label>
-        <input type="password" name="pass" id="pass" />
-        <button role="button" type="submit">OK</button>
+
+        {/* Email input section */}
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" required />
+        
+        {/* Password input section */}
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" required />
+        
+        {/* Submit button */}
+        <button type="submit">OK</button>
       </div>
       <div className="App-footer">
-        <p>Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
+        <p>
+          Copyright {getCurrentYear()} {getFooterCopy(false)}
+        </p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
