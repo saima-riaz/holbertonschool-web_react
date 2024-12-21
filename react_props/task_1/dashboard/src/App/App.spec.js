05795d6e663cb4d8ac8e7691a1c2bf4b18e32a7d
saima-riaz <saimariaz1422@gmail.com>
2024-12-21 23:17:29 +0100

    update code of app.spec.js file

--- react_props/task_1/dashboard/src/App/App.spec.js
+++ react_props/task_1/dashboard/src/App/App.spec.js
@@ -1,45 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders Header component", () => {
+test("renders App components correctly", () => {
   render(<App />);
+
+  // Header component
   const logo = screen.getByAltText(/holberton logo/i);
   expect(logo).toBeInTheDocument();
 
   const heading = screen.getByRole("heading", { level: 1 });
   expect(heading).toHaveTextContent(/school dashboard/i);
-});
 
-test("renders Login component", () => {
-  render(<App />);
-  
-  // Check for the presence of input fields
+  // Login component
   const emailInput = screen.getByRole("textbox");
   const passwordInput = screen.getByLabelText(/password/i);
-  expect(emailInput).toBeInTheDocument();
-  expect(passwordInput).toBeInTheDocument();
-
-  // Check for the presence of labels
   const labelEmail = screen.getByText(/email/i);
   const labelPassword = screen.getByText(/password/i);
+  const button = screen.getByRole("button", { name: /ok/i });
+
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
   expect(labelEmail).toBeInTheDocument();
   expect(labelPassword).toBeInTheDocument();
-
-  // Check for the presence of the button
-  const button = screen.getByRole("button", { name: /ok/i });
   expect(button).toBeInTheDocument();
-});
 
-test("renders Footer component", () => {
-  render(<App />);
+  // Footer component
   const footerElement = screen.getByText(
     new RegExp(`Copyright ${new Date().getFullYear()} - Holberton School`, "i")
   );
   expect(footerElement).toBeInTheDocument();
-});
 
-test("renders Notifications component", () => {
-  render(<App />);
+  // Notifications component
   const notificationsElement = screen.getByText(/here is the list of notifications/i);
   expect(notificationsElement).toBeInTheDocument();
 });
