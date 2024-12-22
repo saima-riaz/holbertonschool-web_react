0a1b9aa7187bd10d1e1055ad087241f60658270d
saima-riaz <saimariaz1422@gmail.com>
2024-12-22 23:12:56 +0100

    fix the error

--- react_props/task_2/dashboard/src/App/App.jsx
+++ react_props/task_2/dashboard/src/App/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import Notifications from '../Notifications/Notifications';
+import Notifications from "../Notifications/Notifications.jsx";
+import Header from "../Header/Header.jsx";
+import Login from "../Login/Login.jsx";
+import Footer from "../Footer/Footer.jsx";
+import { getLatestNotification } from "../utils/utils";
 
-function App() {
-  const notificationsList = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { 
-      id: 3, 
-      type: 'urgent', 
-      html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } 
-    }
-  ];
+const notificationsList = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+];
+
+export default function App() {
 
   return (
-    <div>
-      <Notifications notifications={notificationsList} />
-      {/* Other App content */}
-    </div>
-  );
+    <>
+      <Notifications notifications = {notificationsList} />
+      <Header />
+      <Login />
+      <Footer />
+    </>
+  )
 }
-
-export default App;
