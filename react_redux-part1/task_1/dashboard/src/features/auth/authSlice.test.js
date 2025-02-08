392998e42b270173903c98892debcc8cb4053853
saima-riaz <saimariaz1422@gmail.com>
2025-02-08 01:34:57 +0100

    test updated and fixed

--- react_redux-part1/task_1/dashboard/src/features/auth/authSlice.test.js
+++ react_redux-part1/task_1/dashboard/src/features/auth/authSlice.test.js
@@ -1,6 +1,23 @@
 import authReducer, { login, logout } from './authSlice';
 import { describe, test, expect } from '@jest/globals';
 
+
+const axios = require("axios");
+const AxiosMockAdapter = require("axios-mock-adapter");
+
+// This sets the mock adapter on the default instance
+const mock = new AxiosMockAdapter(axios);
+
+// Mock any GET request to /users
+// arguments for reply are (status, data, headers)
+mock.onGet("/users").reply(200, {
+  users: [{ id: 1, name: "John Smith" }],
+});
+
+axios.get("/users").then(function (response) {
+  console.log(response.data);
+});
+
 describe('authSlice', () => {
   const initialState = {
     user: { email: '', password: '' },
